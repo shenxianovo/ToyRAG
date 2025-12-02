8d8639c8b434ca336d4eade51571ff581ac1e54e
shenxianovo <shenxianovo@outlook.com>
2025-12-02 20:12:26 +0800

    feat: added simple cli for ToyRAG

--- ToyRAG.Cli/Program.cs
+++ ToyRAG.Cli/Program.cs
@@ -1,4 +1,53 @@
-﻿using ToyRAG.Core;
+﻿using ToyRAG.Core.Embeddings;
+using ToyRAG.Core.Generation;
+using ToyRAG.Core.Loading;
+using ToyRAG.Core.Pipelines;
+using ToyRAG.Core.Splitting;
+using ToyRAG.Core.Storage;
 
-var c = new Class1 { Name = "Miku" };
-Console.WriteLine(c.Name);
+string modelPath = @"E:\Code\Local\Learning\C#\ToyRAG\models\all-MiniLM-L12-v2.onnx";
+string vocabPath = @"E:\Code\Local\Learning\C#\ToyRAG\models\vocab.txt";
+string docsPath = @"E:\Code\Local\Learning\C#\ToyRAG\data\docs\standard\serialization";
+
+string gitHubToken = "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";
+
+IDocumentLoader documentLoader = new MarkdownLoader();
+ITextSplitter textSplitter = new FixedTextSplitter();
+IEmbeddingGenerator embeddingGenerator = new OnnxEmbeddingGenerator(modelPath, vocabPath);
+IVectorStore vectorStore = new InMemoryVectorStore();
+IChatService chatService = new GitHubChatService(gitHubToken);
+
+RAGPipeline pipeline = new(
+    documentLoader,
+    textSplitter,
+    embeddingGenerator,
+    vectorStore,
+    chatService
+);
+
+Console.WriteLine($"正在从 {docsPath} 导入文档...");
+var watch = System.Diagnostics.Stopwatch.StartNew();
+await pipeline.ImportAsync(docsPath);
+watch.Stop();
+Console.WriteLine($"导入完成！耗时: {watch.Elapsed.TotalSeconds:F2} 秒");
+
+while (true)
+{
+    Console.Write("\nUser > ");
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.Trim().ToLower() == "exit") break;
+
+    Console.WriteLine("正在思考...");
+    try
+    {
+        string answer = await pipeline.QueryAsync(input);
+        Console.WriteLine("\nAgent :");
+        Console.WriteLine(answer);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\n[错误]: {ex.Message}");
+    }
+}
